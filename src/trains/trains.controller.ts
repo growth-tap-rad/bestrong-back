ebc176cd38a9a94303719a55ecc38091bd2f6506
Grzibovski.1410 <matheusgvieira141@gmail.com>
2023-11-06 15:18:19 -0400

    feat:delete meal e salvando train

--- src/trains/trains.controller.ts
+++ src/trains/trains.controller.ts
@@ -1,13 +1,26 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Controller, Post, Body, Request, Get, UseGuards } from "@nestjs/common";
 import { TrainService } from './trains.service';
 import { TrainDto } from './dtos/train.dto';
-
-@Controller('trains')
+import { AuthGuard } from "src/auth/auth.guard";
+@Controller('me/trains')
 export class TrainsController {
-  constructor(private readonly trainService: TrainService) {}
 
-  @Post('/')
-  async createTrain(@Body() trainsDto: TrainDto) {
-    return this.trainService.createTrain(trainsDto);
+  constructor(private readonly trainService: TrainService) { }
+
+  @UseGuards(AuthGuard)
+  @Post('')
+  async createTrain(
+    @Body() trainsDto: TrainDto,
+    @Request() request: Request,
+  ) {
+    return this.trainService.createTrain(trainsDto, request['user']);
+  }
+  @UseGuards(AuthGuard)
+  @Get('')
+
+  async getTrains(
+    @Request() request: Request,
+  ) {
+    return this.trainService.getTrains(request['user'])
   }
 }
--- src/trains/trains.service.ts
+++ src/trains/trains.service.ts
@@ -3,17 +3,46 @@ import { Repository } from 'typeorm';
 import { Train } from './train.entity';
 import { TrainDto } from './dtos/train.dto';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/user.entity';
 
 @Injectable()
 export class TrainService {
   constructor(
     @InjectRepository(Train)
     private readonly trainsRepository: Repository<Train>,
-  ) {}
-  async createTrain(trainDto: TrainDto) {
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+  ) { }
+  async createTrain(trainDto: TrainDto, user: User) {
     let newTrain = new Train();
+
+    console.log("trainDto ",trainDto, "user ",user)
     Object.assign(newTrain, trainDto);
+
+    const foundUser = await this.userRepository
+
+      .createQueryBuilder('user')
+      .leftJoinAndSelect('user.diary', 'diary')
+      .where('user.id = :userId', { userId: user.id })
+      .orderBy('diary.created_at', 'DESC')
+      .getOne();
+    newTrain.diary = foundUser.diary[0]
+
+
 
     return this.trainsRepository.save(newTrain);
   }
+
+  async getTrains(user: User): Promise<Train[]> {
+
+    return await this.trainsRepository
+      .createQueryBuilder('train')
+      .leftJoin('train.diary', 'diary')
+      .where('diary.userId = :userId', { userId: user.id })
+      .orderBy('diary.id', 'DESC')
+      .getMany();
+
+
+  }
+
 }
