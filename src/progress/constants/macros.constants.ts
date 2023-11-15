0fec36d3aad18f062d897e268d9c23e8ceb058ba
Joao <zacarias1021@gmail.com>
2023-11-15 18:05:28 -0400

    fix: macros calc

--- src/progress/constants/macros.constants.ts
+++ src/progress/constants/macros.constants.ts
@@ -1,56 +1,113 @@
+// export const MACROS = {
+//   low: {
+//     gain: {
+//       protein: 2/7,
+//       carb: 4/7,
+//       fat: 1/7,
+//     },
+//     maintain: {
+//       protein: 2/6,
+//       carb: 2/6,
+//       fat: 2/6,
+//     },
+//     lose: {
+//       protein: 1.6/4,
+//       carb: 1.6/4,
+//       fat: 0.8/4,
+//     },
+//   },
+
+//   moderate: {
+//     gain: {
+//       protein: 2/7,
+//       carb: 4/7,
+//       fat: 1/7,
+//     },
+//     maintain: {
+//       protein: 2/6,
+//       carb: 3/6,
+//       fat: 1/6,
+//     },
+//     lose: {
+//       protein: 2/5.5,
+//       carb: 2.5/5.5,
+//       fat: 1/5.5,
+//     },
+//   },
+
+//   intense: {
+//     gain: {
+//       protein: 2/8,
+//       carb: 5/8,
+//       fat: 1/8,
+//     },
+//     maintain: {
+//       protein: 1.6/7,
+//       carb: 5/7,
+//       fat: 0.4/7,
+//     },
+//     lose: {
+//       protein: 2.2/6.2,
+//       carb: 3/6.2,
+//       fat: 1/6.2,
+//     },
+//   },
+// };
+
+//Testar estes macros
 
 export const MACROS = {
   low: {
     gain: {
-      protein: 2/7,
-      carb: 4/7,
-      fat: 1/7,
+      protein: 2 / 7,
+      carb: 4 / 7,
+      fat: 1 / 7,
     },
     maintain: {
-      protein: 2/6,
-      carb: 2/6,
-      fat: 2/6, 
+      protein: 2 / 6,
+      carb: 2 / 6,
+      fat: 2 / 6,
     },
     lose: {
-      protein: 1.6/4,
-      carb: 1.6/4,
-      fat: 0.8/4,
+      protein: 1.6 / 4,
+      carb: 1.6 / 4,
+      fat: 0.8 / 4,
     },
   },
 
   moderate: {
     gain: {
-      protein: 2/7,
-      carb: 4/7,
-      fat: 1/7,
+      protein: 2 / 7,
+      carb: 4 / 7,
+      fat: 1 / 7,
     },
     maintain: {
-      protein: 2/6,
-      carb: 3/6,
-      fat: 1/6,
+      protein: 2 / 6,
+      carb: 2 / 6,
+      fat: 2 / 6,
     },
     lose: {
-      protein: 2/5.5,
-      carb: 2.5/5.5,
-      fat: 1/5.5,
+      protein: 1.8 / 4.2,
+      carb: 1.6 / 4.2,
+      fat: 0.8 / 4.2,
     },
   },
 
   intense: {
     gain: {
-      protein: 2/8,
-      carb: 5/8,
-      fat: 1/8,
+      protein: 2 / 7,
+      carb: 4 / 7,
+      fat: 1 / 7,
     },
     maintain: {
-      protein: 1.6/7,
-      carb: 5/7,
-      fat: 0.4/7,
+      protein: 2 / 6,
+      carb: 2 / 6,
+      fat: 2 / 6,
     },
     lose: {
-      protein: 2.2/6.2,
-      carb: 3/6.2,
-      fat: 1/6.2,
+      protein: 2.2 / 4.8,
+      carb: 1.6 / 4.8,
+      fat: 1 / 4.8,
     },
   },
 };
