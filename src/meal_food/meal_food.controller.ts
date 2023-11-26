9a5c9ad894bf35bd3c7f18533c282122222f26fb
Joao <zacarias1021@gmail.com>
2023-11-25 22:45:10 -0400

    fix: mealfood

--- src/meal_food/meal_food.controller.ts
+++ src/meal_food/meal_food.controller.ts
@@ -1,26 +1,42 @@
-
-import { Controller, Post, Body, UseGuards, Request, Get, Delete, Param } from "@nestjs/common";
-import { AuthGuard } from "src/auth/auth.guard";
-import { MealFoodService } from "./meal_food.service";
-import { MealFoodDto } from "src/meal_food/dtos/meal_food.dto";
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  Request,
+  Get,
+  Put,
+  Delete,
+  Param,
+} from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { MealFoodService } from './meal_food.service';
+import { MealFoodDto } from 'src/meal_food/dtos/meal_food.dto';
 
 @UseGuards(AuthGuard)
 @Controller('meal_food')
 export class MealFoodController {
-  constructor(private readonly mealFoodService: MealFoodService) { }
+  constructor(private readonly mealFoodService: MealFoodService) {}
 
   @Post('/')
-  async createMealFood(
-    @Body() mealDto: MealFoodDto,
-  ) {
-    return this.mealFoodService.createMealFood(mealDto)
+  async createMealFood(@Body() mealDto: MealFoodDto) {
+    return this.mealFoodService.createMealFood(mealDto);
+  }
+
+  @Put('/:id')
+  async editMealFood(@Body() mealDto: MealFoodDto, @Param('id') id: number) {
+    return this.mealFoodService.editMealFood(mealDto, id);
   }
 
   @Delete('/:id')
-  async deleteMealFood(
-    @Param('id') id: number
-  ) {
+  async deleteMealFood(@Param('id') id: number) {
+    return this.mealFoodService.deleteMealFood(id);
+  }
 
-    return this.mealFoodService.deleteMealFood(id)
+  @Get('/:id')
+  async findMealFood(
+    @Param('id') id: string
+  ) {
+    return this.mealFoodService.getMealFood(id)
   }
 }
